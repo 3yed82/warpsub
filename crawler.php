b5eaf5dfa9fb8025d869712bf707b237fa32ca02
3yed <123330023+3yed82@users.noreply.github.com>
2024-02-23 22:28:29 +0330

    Update crawler.php

--- crawler.php
+++ crawler.php
@@ -7,8 +7,8 @@ $jsonData = json_decode($getList, true);
 $warp = "//profile-title: base64:V0FSUCAoM1lFRPCfkqUp\n";
 $warp .= "//profile-update-interval: 24\n";
 $warp .= "//subscription-userinfo: upload=0; download=0; total=10737418240000000; expire=0\n";
-$warp .= "//support-url: https://github.com/3yed-61\n";
-$warp .= "//profile-web-page-url: https://github.com/3yed-61\n\n";
+$warp .= "//support-url: https://github.com/3yed82\n";
+$warp .= "//profile-web-page-url: https://github.com/3yed82\n\n";
 
 // Loop through the inbounds section
 foreach ($jsonData['inbounds'] as $inbound) {
