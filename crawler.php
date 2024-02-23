8f58ee03280dc1acf82facd3e2e7f01a432aa78a
3yed <123330023+3yed82@users.noreply.github.com>
2024-02-23 23:54:22 +0330

    Update crawler.php

--- crawler.php
+++ crawler.php
@@ -1,35 +1,24 @@
-$url = 'https://raw.githubusercontent.com/ippscan/ippscanTEAM/main/Montervpn';
+<?php
 
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $getList = file_get_contents('https://raw.githubusercontent.com/ippscan/ippscanTEAM/main/Montervpn?v1.'.time());
+    $strings = explode("\n", $getList);
 
-$getList = curl_exec($ch);
+    $warp = "//profile-title: base64:V0FSUCAoM1lFRPCfkqUp\n";
+    $warp .= "//profile-update-interval: 24\n";
+    $warp .= "//subscription-userinfo: upload=0; download=0; total=10737418240000000; expire=0\n";
+    $warp .= "//support-url: https://github.com/3yed821\n";
+    $warp .= "//profile-web-page-url: https://github.com/3yed82\n\n";
 
-if (curl_errno($ch)) {
-    die('Unable to fetch content from the URL: ' . curl_error($ch));
-}
-
-curl_close($ch);
-
-$strings = explode("\n", $getList);
-
-$warp = "//profile-title: base64:V0FSUCAoM1lFRPCfkqUp\n";
-$warp .= "//profile-update-interval: 24\n";
-$warp .= "//subscription-userinfo: upload=0; download=0; total=10737418240000000; expire=0\n";
-$warp .= "//support-url: https://github.com/3yed821\n";
-$warp .= "//profile-web-page-url: https://github.com/3yed82\n\n";
-
-
-$i = 1;
-$pattern = '/^warp:\/\/.*$/';
-foreach ($strings as $val) {
-    if ($i > 3) {
-        break;
+    $i = 1;
+    $pattern = '/^warp:\/\/.*$/';
+    foreach ($strings as $val) {
+        if ( $i > 3) {
+            break;
+        }
+        if (preg_match($pattern, $val) && !str_contains($val, '&&detour=')) {
+            $warp .= "\n".str_replace(['🇮🇷', '🛡', '✔️', '⭐️', '✅'], '', $val);
+            $i++;
+        }
     }
-    if (preg_match($pattern, $val) && !str_contains($val, '&&detour=')) {
-        $warp .= "\n" . str_replace(['🇮🇷', '🛡', '✔️', '⭐️', '✅'], '', $val);
-        $i++;
-    }
-}
 
-file_put_contents("export/warp", $warp);
+    file_put_contents("export/warp", $warp);
