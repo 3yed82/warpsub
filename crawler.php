7211b0b296578182db56009c111ef7cfa12424c1
3yed <123330023+3yed82@users.noreply.github.com>
2024-02-29 19:34:16 +0330

    Update crawler.php

--- crawler.php
+++ crawler.php
@@ -1,12 +1,12 @@
 <?php
 
-    $getList = file_get_contents('https://raw.githubusercontent.com/ippscan/ippscanTEAM/main/Montervpn?v1.'.time());
+    $getList = file_get_contents('https://raw.githubusercontent.com/nameless4pub/warpsub/main/export/warp?v1.'.time());
     $strings = explode("\n", $getList);
 
     $warp = "//profile-title: base64:V0FSUCAoM1lFRPCfkqUp\n";
     $warp .= "//profile-update-interval: 24\n";
     $warp .= "//subscription-userinfo: upload=0; download=0; total=10737418240000000; expire=0\n";
-    $warp .= "//support-url: https://github.com/3yed821\n";
+    $warp .= "//support-url: https://github.com/3yed82\n";
     $warp .= "//profile-web-page-url: https://github.com/3yed82\n\n";
     $warp .= "warp://auto#🇮🇷 WARP &&detour=warp://auto#🇩🇪 WARP";
 
