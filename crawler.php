ef1ae934585e283af69785f8fde813b79e0955e1
3yed <123330023+3yed82@users.noreply.github.com>
2024-03-08 14:53:47 +0330

    Update crawler.php

--- crawler.php
+++ crawler.php
@@ -3,10 +3,10 @@
     $getList = file_get_contents('https://raw.githubusercontent.com/nameless4pub/warp-ip/main/warp.json?v1.'.time());
     $strings = explode("\n", $getList);
 
-    $warp = "//profile-title: base64:" + base64.b64encode('3yed82⭐️'.encode('utf-8')).decode('utf-8') + "\n"
-    $warp = "//profile-update-interval: 1\n"
-    $warp = "//subscription-userinfo: upload=0; download=0; total=10737418240000000; expire=2546249531\n"
-    $warp = "//profile-web-page-url: https://github.com/3yed82\n"
+    $warp = "//profile-title: base64:V0FSUHwzWUVE4q2Q77iP\n";
+    $warp .= "//profile-update-interval: 24\n";
+    $warp .= "//subscription-userinfo: upload=0; download=0; total=10737418240000000; expire=0\n";
+    $warp .= "//profile-web-page-url: https://github.com/3yed82\n\n";
     $warp .= "warp://auto#🇮🇷 WARP &&detour=warp://auto#🇩🇪 WARP
 
     $i = 1;
