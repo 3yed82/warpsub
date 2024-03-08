13d258588dc169b9d12983789d1d2b7c84e91b9e
3yed <123330023+3yed82@users.noreply.github.com>
2024-03-08 15:12:57 +0330

    Update crawler.php

--- crawler.php
+++ crawler.php
@@ -3,21 +3,23 @@
     $getList = file_get_contents('https://raw.githubusercontent.com/nameless4pub/warpsub/main/export/warp?v1.'.time());
     $strings = explode("\n", $getList);
 
-    $warp = "//profile-title: base64:V0FSUHwzWUVE4q2Q77iP\n";
+    $warp = "//profile-title: base64:V0FSUCAoM1lFRCk=\n";
     $warp .= "//profile-update-interval: 24\n";
     $warp .= "//subscription-userinfo: upload=0; download=0; total=10737418240000000; expire=0\n";
-    $warp .= "//profile-web-page-url: https://github.com/3yed82\n\n";
-    $warp .= "warp://auto#🇮🇷 WARP &&detour=warp://auto#🇩🇪 WARP
+    $warp .= "//support-url: https://github.com/3yed-61\n";
+    $warp .= "//profile-web-page-url: https://github.com/3yed-61\n\n";
+    $warp .= "warp://auto#🇮🇷 WARP &&detour=warp://auto#🇩🇪 WARP";
 
     $i = 1;
     $pattern = '/^warp:\/\/.*$/';
     foreach ($strings as $val) {
-    if ($i > 2) {
-        break;
+        if ( $i > 3) {
+            break;
+        }
+        if (preg_match($pattern, $val) && !str_contains($val, '&&detour=')) {
+            $warp .= "\n".str_replace(['🇮🇷', '🛡', '✔️', '⭐️', '✅'], '', $val);
+            $i++;
+        }
     }
-    if (preg_match($pattern, $val) && !str_contains($val, '&&detour=')) {
-        $warp .= "\n" . $val . '#Warp 🇮🇷 IP&&detour=' . $val . '#Warp 🇩🇪 IP';
-        $i++;
-    }
-}
-     file_put_contents("export/warp", $warp);
+
+    file_put_contents("export/warp", $warp);
